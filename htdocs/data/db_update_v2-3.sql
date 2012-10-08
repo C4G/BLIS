1803ef86fc261af6ea375a0d825fa947e63b5ac8
Robert Thackston <Robert Thackston@192.168.94.128>
2012-10-08 17:33:01 -0400

    Added DB tables for invoices and invoice line items.

---
+++ htdocs/data/db_update_v2-3.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `invoices` (
+  `id` int(11) NOT NULL auto_increment,
+  `amount` decimal(10,2) NOT NULL default '0',
+  `patient_id` int(11) NOT NULL,
+  `date` date default NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `patient_id` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+
+CREATE TABLE `invoice_line_items` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(100) NOT NULL,
+  `amount` decimal(10,2) NOT NULL default '0',
+  `invoice_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `invoice_id` FOREIGN KEY (`invoice_id`) REFERENCES `invoices` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
