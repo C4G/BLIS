ae8dafb62424977e907a5b61d364c3782e4da214
Robert Thackston <robert.thackston@gmail.com>
2012-10-17 00:32:50 -0400

    Getting database schemas ready for invoices

--- htdocs/data/db_update_v2-3.sql
+++ htdocs/data/db_update_v2-3.sql
@@ -1,21 +1,23 @@
-CREATE TABLE `invoices` (
-  `id` int(11) NOT NULL auto_increment,
-  `amount` decimal(10,2) NOT NULL default '0',
-  `patient_id` int(11) NOT NULL,
-  `date` date default NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `patient_id` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+CREATE TABLE `patient_bills` (
+	`bill_id` int(11) auto_increment NOT NULL,
+	`bill_amount` decimal(10,2) NOT NULL DEFAULT '0',
+	`bill_date` date NOT NULL,
+	`patient_id` int(11) UNSIGNED NOT NULL,
+	PRIMARY KEY(`bill_id`),
+        CONSTRAINT `patient_id` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`patient_id`) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
-
-CREATE TABLE `invoice_line_items` (
+CREATE TABLE `test_type_costs` (
   `id` int(11) NOT NULL auto_increment,
-  `name` varchar(100) NOT NULL,
   `amount` decimal(10,2) NOT NULL default '0',
-  `invoice_id` int(11) NOT NULL,
+  `test_type_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `invoice_id` FOREIGN KEY (`invoice_id`) REFERENCES `invoices` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+  CONSTRAINT `test_type_id` FOREIGN KEY (`test_type_id`) REFERENCES `test_types` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
-ALTER TABLE `test_type`
-  ADD `costToPatient` decimal(10,2) NOT NULL default '0';
+CREATE TABLE `tests_bills_association` (
+	`id` int(11) UNSIGNED auto_increment NOT NULL,
+	`patient_bill_id` int(11) UNSIGNED NOT NULL,
+	`test_id` int(11) UNSIGNED NOT NULL,
+	PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
