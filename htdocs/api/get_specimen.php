3f1c332f8c470555fd53567a39da62564bfda258
madhavanmurrali <madhavanmurrali@gmail.com>
2014-12-09 11:01:59 -0500

    Commiting the changes in get sepecimen file for bug fix

--- htdocs/api/get_specimen.php
+++ htdocs/api/get_specimen.php
@@ -1,25 +1,17 @@
 <?php
 
 include "../includes/db_lib.php";
-
-$username = @$_REQUEST['username'];
-$password = @$_REQUEST['password'];
-$dbname = API::login($username, $password);
-if($dbname === -1)
+if(!isset($_REQUEST['specimen_id']))
 {
-	echo -1;exit;
+    echo -2;
+    return;
 }
-	$specimen_id = @$_REQUEST['specimen_id'];
-	$specimen_type_filter = @$_REQUEST['specimenfilter'];
-	$test_filter = @$_REQUEST['testfilter'];
-	$datefrom = @$_REQUEST['datefrom'];
-	$dateto = @$_REQUEST['dateto'];
-	$result = API::get_specimenAndTest($dbname,$specimen_id,$specimen_type_filter,$test_filter,$datefrom,$dateto);
-	
-	if($result < 1)
-		echo $result;
-	else
-		echo json_encode($result); 
 
+$specimen_id = $_REQUEST['specimen_id'];
+$result = API::get_specimen($specimen_id);
 
+if($result < 1)
+    echo $result;
+else
+    echo json_encode($result); 
 ?>
