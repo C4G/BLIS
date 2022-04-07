d1d3a8c09c933cbd99623939e22049a703160435
Mitchell Rysavy <mrysav@github.com>
2022-04-07 01:30:15 +0000

    Run php-cs-fixer on blis_update.php

--- htdocs/update/blis_update.php
+++ htdocs/update/blis_update.php
@@ -5,7 +5,7 @@
 # Calls ajax/update.php which actually performs the update operations
 
 /*include("../users/accesslist.php");
-if( !(isCountryDir(get_user_by_id($_SESSION['user_id'])) && in_array(basename($_SERVER['PHP_SELF']), $countryDirPageList)) 
+if( !(isCountryDir(get_user_by_id($_SESSION['user_id'])) && in_array(basename($_SERVER['PHP_SELF']), $countryDirPageList))
     && !(isSuperAdmin(get_user_by_id($_SESSION['user_id'])) && in_array(basename($_SERVER['PHP_SELF']), $superAdminPageList)) &&
     !(isAdmin(get_user_by_id($_SESSION['user_id'])) && in_array(basename($_SERVER['PHP_SELF']), $adminPageList)) )
     header( 'Location: home.php' );
@@ -26,7 +26,7 @@ $query = "SELECT max(version) version FROM version_data";
 $record = query_associative_one($query);
 DbUtil::switchRestore($saved_db);
 
-$vers = $record['version']; 
+$vers = $record['version'];
 
 $version_list = array("2.2","2.3","2.4","2.5","2.6","2.7","2.8","2.9","2.91","3.0","3.1","3.2","3.21","3.3", "3.4","3.5","3.51","3.52","3.6", "3.7", "3.71", "3.72");
 
@@ -39,25 +39,22 @@ $retstr2 = "";
 $retstr3 = "";
 
 $v_index = 0;
-while($v_index<count($version_list)) {
-    if($version_list[$v_index] == $vers)
-    {
+while ($v_index<count($version_list)) {
+    if ($version_list[$v_index] == $vers) {
         $v_index++;
-        while($v_index<count($version_list)){
-            if ( is_super_admin($user) || is_country_dir($user) ) {
-                if($version_doc_admin[$v_index] != ""){
+        while ($v_index<count($version_list)) {
+            if (is_super_admin($user) || is_country_dir($user)) {
+                if ($version_doc_admin[$v_index] != "") {
                     $db_name = "blis_revamp";
                     $ufile = $version_doc_admin[$v_index];
                     blis_db_update($lab_config_id, $db_name, $ufile);
-                    $retstr1 =  $retstr1.$version_doc_admin[$v_index]; 
+                    $retstr1 =  $retstr1.$version_doc_admin[$v_index];
                 }
-                
-            }
-            else {
+            } else {
                 $lab_config_id = $_SESSION['lab_config_id'];
 
                 # revamp update
-                if($version_doc_revamp[$v_index] != ""){
+                if ($version_doc_revamp[$v_index] != "") {
                     $db_name = "blis_revamp";
                     $ufile =  $version_doc_revamp[$v_index];
                     blis_db_update($lab_config_id, $db_name, $ufile);
@@ -65,7 +62,7 @@ while($v_index<count($version_list)) {
                 }
 
                 # lab update
-                if($version_doc_lab[$v_index] != ""){
+                if ($version_doc_lab[$v_index] != "") {
                     $db_name = "blis_".$lab_config_id;
                     $ufile = $version_doc_lab[$v_index];
                     blis_db_update($lab_config_id, $db_name, $ufile);
@@ -81,11 +78,10 @@ while($v_index<count($version_list)) {
     $v_index++;
 }
 
-if ( is_super_admin($user) || is_country_dir($user) ) {
+if (is_super_admin($user) || is_country_dir($user)) {
     update_language_files();
-    // insertVersionDataEntry();
-}
-else {
+// insertVersionDataEntry();
+} else {
     $lab_config_id = $_SESSION['lab_config_id'];
     update_language_files();
     $def = default_currency_copy($lab_config_id);
@@ -102,5 +98,4 @@ query_insert_one($query_string);
 DbUtil::switchRestore($saved_db);
 
 // echo $retstr1.$retstr2.$retstr3;
-echo "true"
-?>
+echo "true";
