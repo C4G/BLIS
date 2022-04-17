6973805c34eef25f7106c61057c6f53f4e9dbf48
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2022-04-16 20:49:23 -0400

    Clean up some env changes

--- Dockerfile
+++ Dockerfile
@@ -24,13 +24,18 @@ RUN add-apt-repository ppa:ondrej/php && apt-get update && \
         php5.6-mbstring \
         php5.6-xml
 
+# This is a mysqldump configuration option required in this environment to
+# preserve backwards compatibility with earlier versions of mysqldump
 RUN echo "column-statistics = 0" | tee -a /etc/mysql/conf.d/mysqldump.cnf
 
 # Copy the custom Apache2 config (blis.conf) into the 
 # Apache2 configuration directory. This will be enabled by the start-blis.sh
 COPY docker/config/blis-release.conf /etc/apache2/blis-release.conf
-ENV BLIS_APACHE2_CONFIG /etc/apache2/sites-available/blis-release.conf
 
+# Enable Apache2 modules needed by application and disable default site
+RUN a2enmod rewrite socache_shmcb ssl && a2dissite 000-default
+
+# Copy custom PHP config into the container
 COPY docker/config/php.ini /etc/php/5.6/apache2/php.ini
 
 # Copy utility scripts to /usr/bin
@@ -38,10 +43,12 @@ COPY docker/bin/set-apache2-servername.py /usr/bin/
 COPY docker/bin/get-https-cert.sh /usr/bin/
 COPY docker/bin/start-blis.sh /usr/bin/
 
+# Copy all of the BLIS files into the container
 RUN mkdir /var/www/blis
 COPY . /var/www/blis
-RUN chown -R www-data:www-data /var/www/blis
+RUN chown -R www-data:www-data /var/www
 
+# Expose port 80 for HTTP, 443 for HTTPS (optionally)
 EXPOSE 80
 EXPOSE 443
 
--- docker/bin/start-blis.sh
+++ docker/bin/start-blis.sh
@@ -9,26 +9,27 @@ if ! grep -q 'apache2_blis.env' /etc/apache2/envvars; then
     echo ". /var/www/blis/files/apache2_blis.env" >> /etc/apache2/envvars
 fi
 
+# This is a little funky...
+# The container build puts the config in /etc/apache2, but this is not the correct place for it.
+# The reason for this is so you can mount a volume to /etc/apache2/sites-available to persist your changes.
+# When the container starts, it will only overwrite the configuration with the default if the file doesn't
+# exist already.
 if [[ ! -f "/etc/apache2/sites-available/blis-release.conf" ]]; then
     if [[ -f "/etc/apache2/blis-release.conf" ]]; then
         echo -e "Thanks for setting up BLIS! Just getting some things in place..."
         mv /etc/apache2/blis-release.conf /etc/apache2/sites-available/blis-release.conf
+        # Set the ServerName properly, according to the environment
+        BLIS_APACHE2_CONFIG="/etc/apache2/sites-available/blis-release.conf" set-apache2-servername.py
     else
         echo "The blis-release.conf Apache2 configuration is not present. Did the container build correctly?"
         exit 1
     fi
 fi
 
-set-apache2-servername.py
-
-a2enmod rewrite
-
-a2ensite blis-release
-
 if [[ -f "/etc/apache2/sites-available/blis-release-le-ssl.conf" ]]; then
-    echo "Detected an SSL configuration... enabling Apache2 modules"
-    a2enmod socache_shmcb ssl
-    a2ensite blis-release-le-ssl
+    echo "Detected an SSL configuration!"
 fi
+
+a2ensite blis-release*
 
 service apache2 start
