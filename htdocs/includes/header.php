6162e09518ff59dcd0cfd9c82f043d72e07d48ed
Mitchell Rysavy <mrysav@github.com>
2022-04-29 01:25:39 +0000

    fix header syntax

--- htdocs/includes/header.php
+++ htdocs/includes/header.php
@@ -11,8 +11,7 @@ require_once("includes/db_lib.php");
 
 $TRACK_LOADTIME = false;
 $TRACK_LOADTIMEJS = false;
-if($TRACK_LOADTIME)
-{
+if ($TRACK_LOADTIME) {
     $starttime = microtime();
     $startarray = explode(" ", $starttime);
     $starttime = $startarray[1] + $startarray[0];
@@ -40,17 +39,16 @@ $page_elems = new PageElems();
         $script_elems->enableFacebox();
         $script_elems->enableAutoScrollTop();
         $script_elems->enableMultiSelect();
-        if(strpos($_SERVER['PHP_SELF'], "login.php") === false)
-        {	
-            if($AUTO_LOGOUT === true)
+        if (strpos($_SERVER['PHP_SELF'], "login.php") === false) {
+            if ($AUTO_LOGOUT === true) {
                 $script_elems->enableAutoLogout();
+            }
         }
         ?>
 
     <script type='text/javascript'>
-    <?php 
-    if($TRACK_LOADTIMEJS)
-    {
+    <?php
+    if ($TRACK_LOADTIMEJS) {
         echo "var t = new Date();";
     }
     ?>
@@ -68,31 +66,25 @@ $page_elems = new PageElems();
     <div id="top_pane">
         <div id="top_pane_user_info">
 <?php
-if(isset($_SESSION['username']))
-{
-?>
-    <?php echo LangUtil::getPageTerm("LOGGEDINAS"); ?>: <?php echo $_SESSION['username'];?> | 
+if (isset($_SESSION['username'])) {
+        ?>
+    <?php echo LangUtil::getPageTerm("LOGGEDINAS"); ?>: <?php echo $_SESSION['username']; ?> | 
     <a href='edit_profile.php' class="black"><?php echo LangUtil::$pageTerms['EDITPROFILE']; ?></a> | 
     <?php
     //echo "test".$_SESSION['admin_as_tech'];
-    if(isset($_SESSION['admin_as_tech']) && $_SESSION['admin_as_tech'] === true)
-    {
+    if (isset($_SESSION['admin_as_tech']) && $_SESSION['admin_as_tech'] === true) {
         ?>
         <a href='switchto_admin.php' class="black"><?php echo LangUtil::getPageTerm("SWITCH_TOMGR"); ?></a> | 
         <?php
-    }
-    else if(isset($_SESSION['dir_as_tech']) && $_SESSION['dir_as_tech'] === true)
-    {
+    } elseif (isset($_SESSION['dir_as_tech']) && $_SESSION['dir_as_tech'] === true) {
         ?>
         <a href='switchto_admin.php' class="black"><?php echo LangUtil::getPageTerm("SWITCH_TODIR"); ?></a> | 
         <?php
-    }
-    else if(User::onlyOneLabConfig($_SESSION['user_id'], $_SESSION['user_level']))
-    {
-        $lab_config_list = get_lab_configs($_SESSION['user_id']);
-        ?>
+    } elseif (User::onlyOneLabConfig($_SESSION['user_id'], $_SESSION['user_level'])) {
+        $lab_config_list = get_lab_configs($_SESSION['user_id']); ?>
         <a href='switchto_tech.php?id=<?php echo $lab_config_list[0]->id; ?>' class="black"><?php echo LangUtil::getPageTerm("SWITCH_TOTECH"); ?></a> | 
         <?php
+    }
     }
     ?>
     <a rel='facebox' href='user_rating.php' class="black"><?php echo LangUtil::getPageTerm("LOGOUT"); ?></a>	
@@ -109,49 +101,42 @@ if(isset($_SESSION['username']))
             </tr>
         </table>
 <?php
-if(strpos($_SERVER['PHP_SELF'], 'login.php') === false)
-{
-?>
+if (strpos($_SERVER['PHP_SELF'], 'login.php') === false) {
+        ?>
     <div id="menus">
     <ul id='globalnav'>
 <?php
     // echo "hi <br>";
     // echo $top_menu_options;
     // echo "<br>";
-    if(isset($top_menu_options))
-    {
-        foreach($top_menu_options as $key => $value)
-        {
+    if (isset($top_menu_options)) {
+        foreach ($top_menu_options as $key => $value) {
             //echo "hello "."<br/>";
             
             echo "<li ";
             echo "><a href='".$value."' ";
-            if(
+            if (
                 (strpos($_SERVER['PHP_SELF'], $value) !== false)
                 && !(strpos($_SERVER['PHP_SELF'], "_home.php") !== false && $value == "home.php")
-            )
-            {
+            ) {
                 # Highlight current page tab
                 echo " class='here' ";
             }
-            if(strpos($key, LangUtil::$pageTerms['MENU_BACKUP']) !== false)
-            {
-//				echo " target='_blank' ";
+            if (strpos($key, LangUtil::$pageTerms['MENU_BACKUP']) !== false) {
+                //				echo " target='_blank' ";
             }
-            if(strpos($_SERVER['PHP_SELF'], "_home.php") !== false && strpos($value, "lab_configs.php") !== false)
-            {
+            if (strpos($_SERVER['PHP_SELF'], "_home.php") !== false && strpos($value, "lab_configs.php") !== false) {
                 echo " class='here' ";
             }
             echo ">".$key."</a></li>";
         }
-    }
-?>
+    } ?>
     <span id='backup_div' style='float:right;margin-right:15px;'>
     </span>
     </ul>
     </div>
 <?php
-}
+    }
 ?>
 </div><!--end of top_pane-->
 <div id='center_pane'>
