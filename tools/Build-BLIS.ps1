e563ca40e8698607b4f59c553acb8693567838b4
Mitchell Rysavy <mrysav@github.com>
2022-05-03 00:19:20 +0000

    Add Build-BLIS.ps1 script for making a BLIS-on-Windows release

---
+++ tools/Build-BLIS.ps1
@@ -0,0 +1,26 @@
+New-Item -ItemType Directory -Path "Build-BLIS/" -Force
+Push-Location -Path "Build-BLIS/"
+
+$runtime_url = "https://github.com/c4g-spr22-blis/BLISRuntime/archive/refs/heads/main.zip"
+$code_url = "https://github.com/c4g-spr22-blis/BLIS/archive/refs/heads/master.zip"
+
+Write-Host "Downloading BLIS Runtime..."
+Invoke-WebRequest -Uri $runtime_url -OutFile "BLISRuntime.zip"
+
+Write-Host "Downloading BLIS code..."
+Invoke-WebRequest -Uri $code_url -OutFile "BLISCode.zip"
+
+Expand-Archive "BLISRuntime.zip" -DestinationPath "."
+Expand-Archive "BLISCode.zip" -DestinationPath "."
+
+Move-Item -Path "BLIS-master" -Destination "BLIS"
+Get-ChildItem -Path "BLISRuntime-main" | Move-Item -Destination "BLIS/"
+
+Compress-Archive -Path BLIS -DestinationPath BLIS.zip
+
+Pop-Location
+
+Move-Item -Path "Build-BLIS/BLIS.zip" -Destination BLIS.zip
+Remove-Item -Force -Path "Build-BLIS"
+
+Write-Host -ForegroundColor Green "Done!"
