c167f63eb092a8af5559c41f0fd90ba19769ae59
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2022-05-09 17:16:39 -0400

    Add update-lang.php, update default default.php

---
+++ bin/update-lang.php
@@ -0,0 +1,27 @@
+#!/usr/bin/env php
+
+<?php
+
+require_once(__DIR__."/../htdocs/lang/lang_xml2php.php");
+
+if ($argc < 2) {
+    echo("You must supply the path to an XML file to generate the PHP language file for.\n");
+    die(1);
+}
+
+$xml_path = realpath($argv[1]);
+$info = pathinfo($xml_path);
+
+$locale = $info['filename'];
+$dirname = $info['dirname'];
+
+$php_path = "$dirname/$locale.php";
+
+echo("Generating PHP file: $php_path\n");
+echo("From XML file:       $xml_path\n");
+
+echo("Calling: lang_xml2php(\"$locale\", \"$dirname/\")\n");
+lang_xml2php($locale, "$dirname/");
+
+echo("Calling: require_once(\"$php_path\") to ensure valid PHP syntax...\n");
+require_once($php_path);
