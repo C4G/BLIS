408e8dfaee6e25575ac55ddd7bdb45adb4133bfd
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2024-04-14 19:46:38 +0000

    Fix path in post-create and add step names

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+
+echo "export PATH=\"$HOME/.local/bin:\$PATH\"" | tee -a "$HOME/.bashrc"
+# shellcheck disable=SC1091
+source "$HOME/.bashrc"
 
 pip3 install -r requirements.txt
