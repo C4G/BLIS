195dc4300360004503c794961c9abfd715c6db5e
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2024-09-27 13:50:09 +0000

    WIP

--- htdocs/includes/features.php
+++ htdocs/includes/features.php
@@ -13,6 +13,10 @@ class Features {
         // return Features::ev_enabled("BLIS_LAB_BACKUPS_V2_ENABLED");
     }
 
+    public static function lab_connection_enabled() {
+        return Features::ev_enabled("BLIS_LAB_CONNECTION_ENABLED");
+    }
+
     private static function ev_enabled($ev) {
         return getenv($ev) == "1" || getenv($ev) == "true";
     }
