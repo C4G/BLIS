4475e0a17740d5b73b200be381875ddf616a9124
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2024-11-06 09:50:04 -0500

    End to end works without encryption (Windows side)

--- htdocs/config/v2/send_cloud_backup.php
+++ htdocs/config/v2/send_cloud_backup.php
@@ -9,9 +9,10 @@ $current_user_id = $_SESSION['user_id'];
 $current_user = get_user_by_id($current_user_id);
 
 $lab_config_id = $_REQUEST['lab_config_id'];
-$lab_config_query = "SELECT lab_config_id, blis_cloud_hostname, blis_cloud_connection_key, blis_cloud_server_pubkey_id FROM lab_config WHERE lab_config_id = '$lab_config_id';";
+$lab_config_query = "SELECT db_name, lab_config_id, blis_cloud_hostname, blis_cloud_connection_key, blis_cloud_server_pubkey_id FROM lab_config WHERE lab_config_id = '$lab_config_id';";
 db_change("blis_revamp");
 $lab = query_associative_one($lab_config_query);
+$lab_db_name = $lab['db_name'];
 
 $unauthorized = true;
 
@@ -37,61 +38,68 @@ $connect_url = $lab["blis_cloud_hostname"];
 $connect_code = $lab["blis_cloud_connection_key"];
 $server_key_id = $lab["blis_cloud_server_pubkey_id"];
 
-$log->warn(json_encode($lab));
-
 $server_pubkey = KeyMgmt::getById($server_key_id);
-$server_pubkey_text = $server_pubkey->PubKey;
-$server_pubkey_text = str_replace(" ", "\n", $server_pubkey_text);
-$log->warn($server_pubkey_text);
+$server_pubkey_text = base64_decode($server_pubkey->PubKey);
 $ossl_pubkey = openssl_get_publickey($server_pubkey_text);
-if ($ossl_pubkey == false) {
-    $log->error("Could not get server pubkey: " . openssl_error_string());
-    $_SESSION["FLASH"] = "Error sending backup.";
-    header("Location: /lab_config_home.php?id=$lab_config_id");
-    exit();
-}
+// if ($ossl_pubkey === false) {
+//     $log->error("Could not get server pubkey: " . openssl_error_string());
+//     $_SESSION["FLASH"] = "Error sending backup.";
+//     header("Location: /lab_config_home.php?id=$lab_config_id");
+//     exit();
+// }
 
+db_change($lab_db_name);
 $lab_backups = Backup::for_lab_config_id($lab_config_id);
 $latest_backup = $lab_backups[0];
+db_change("blis_revamp");
 
 $backup_blob = file_get_contents($latest_backup->full_path);
 
-$ossl_result = openssl_seal($backup_blob, $sealed_data, $ekeys, array($ossl_pubkey), "AES256", $iv);
-if ($ossl_result == false) {
-    $log->error("Could not encrypt backup: " . openssl_error_string());
-    $_SESSION["FLASH"] = "Error sending backup.";
-    header("Location: /lab_config_home.php?id=$lab_config_id");
-    exit();
-}
+// TODO: Why does encryption not work??
 
-$tmpfil = tmpfile();
-$tmpmeta = stream_get_meta_data($tmpfil);
-$tmppath = $tmpmeta['uri'];
-fwrite($tmpfil, $sealed_data);
-fseek($tmpfil, 0);
+// $ossl_result = openssl_seal($backup_blob, $sealed_data, $ekeys, array($ossl_pubkey), "AES256", $iv);
+// if ($ossl_result == false) {
+//     $log->error("Could not encrypt backup: " . openssl_error_string());
+//     $_SESSION["FLASH"] = "Error sending backup.";
+//     header("Location: /lab_config_home.php?id=$lab_config_id");
+//     exit();
+// }
+
+// $tmpfil = tmpfile();
+// $tmpmeta = stream_get_meta_data($tmpfil);
+// $tmppath = $tmpmeta['uri'];
+// fwrite($tmpfil, $sealed_data);
+// fseek($tmpfil, 0);
+
+// copy($tmppath, "C:\Users\beatl\Desktop\myzip.enc");
+// $log->warn(base64_encode($iv));
+// $log->warn(base64_encode($ekeys[0]));
+
+$curl = curl_init();
 
 if (function_exists('curl_file_create')) {
     // For PHP 5.5+, which is what we use in the BLIS docker image
-    $curlfile = curl_file_create($tmppath, 'application/octet-stream');
+    $curlfile = curl_file_create($latest_backup->full_path, 'application/octet-stream');
 } else {
     // For old-school PHP, which is in-use by the Desktop BLIS
-    $curlfile = '@' . realpath($tmppath);
+    $curlfile = '@' . realpath($latest_backup->full_path);
     $log->info("Resolved path to: $curlfile");
 }
 
+$iv="";
 $post = array('action'=>'backup', 'connection_code'=>$connect_code,'backup_file'=>$curlfile,'envelope_key'=>$iv);
 
 $log->info("Sending backup to BLIS cloud with URL: ".$connect_url);
 
-$curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, $connect_url);
 curl_setopt($curl, CURLOPT_POST, 1);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 $response = curl_exec($curl);
+$httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 curl_close($curl);
 
-fclose($tmpfil);
+// fclose($tmpfil);
 
 $_SESSION['FLASH'] = "Backup sent successfully.";
-header("Location: /lab_config_home.php");
+header("Location: /lab_config_home.php?id=$lab_config_id");
