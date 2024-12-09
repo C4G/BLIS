cc9b5898632fd2d0886d6da5eefcb1f096fcb70d
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2024-12-09 03:42:52 +0000

    Use curl.exe for connection flow as well

--- htdocs/config/v2/connect_to_blis_cloud.php
+++ htdocs/config/v2/connect_to_blis_cloud.php
@@ -2,6 +2,7 @@
 
 require_once(__DIR__."/../../includes/composer.php");
 require_once(__DIR__."/../../includes/keymgmt.php");
+require_once(__DIR__."/../../includes/platform_lib.php");
 require_once(__DIR__."/../../includes/user_lib.php");
 
 $current_user_id = $_SESSION['user_id'];
@@ -41,54 +42,75 @@ $connect_code = $_REQUEST['blis-cloud-code'];
 $p_url = parse_url($connect_url);
 $is_localhost = $p_url["host"] == "localhost";
 $is_localnetwork = substr($p_url["host"], 0, 8) == "192.168.";
-
-// WARNING! This _should_ get turned on again.
-// Unfortunately, the version of Apache/PHP/OpenSSL on the client BLIS only supports
-// the insecure and outdated TLS 1.1, which isn't supported by modern webservers (due to the insecurity)
-// Rather than decrease security for all traffic, we'll just allow HTTP servers here... sigh...
-// if ($p_url["scheme"] != "https" && !$is_localhost && !$is_localnetwork) {
-//     $_SESSION["FLASH"] = "You must specify a secure URL (starting with https://) for BLIS Cloud.";
-//     header('HTTP/1.1 400 Bad Request', true, 400);
-//     header("Location: /lab_config_home.php?id=$lab_config_id");
-//     exit;
-// }
+if ($p_url["scheme"] != "https" && !$is_localhost && !$is_localnetwork) {
+    $_SESSION["FLASH"] = "You must specify a secure URL (starting with https://) for BLIS Cloud.";
+    header('HTTP/1.1 400 Bad Request', true, 400);
+    header("Location: /lab_config_home.php?id=$lab_config_id");
+    exit;
+}
 
 $post = array('action'=> 'connect', 'public_key'=> '', 'lab_name'=>$lab_config_name, 'connection_code'=> $connect_code);
 
 $log->info("Connecting to BLIS cloud with URL: ".$connect_url);
 
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, $connect_url);
-curl_setopt($curl, CURLOPT_POST, 1);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-$response = curl_exec($curl);
-$httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+$curl_cmd = PlatformLib::curlPath();
+$curl_cmd = $curl_cmd . " -i --fail-with-body --show-error -X POST ";
+foreach($post as $key=>$value) {
+    $curl_cmd = $curl_cmd . " -F \"$key=$value\"";
+}
+$curl_cmd = $curl_cmd . " $connect_url";
 
-if ($httpCode != 200) {
-    $_SESSION['FLASH'] = "There was an error establishing the connection. Check the logs for details.";
-    $log->error("Error connecting to server. URL: $connect_url, response status: $httpCode; response: $response; curl: ".curl_error($curl));
-} else {
-    $r_json = json_decode($response, true);
+$NUM_RETRIES = 5;
 
-    $new_conncode = $r_json["connection_code"];
-    $server_pubkey = base64_decode($r_json["public_key"]);
+for ($x = 0; $x < $NUM_RETRIES; $x = $x + 1) {
+    $log->info("Attempt #" . ($x + 1));
 
-    $key_lab_name = "BLIS Cloud Connection for $lab_config_name";
-    $key = KeyMgmt::create($key_lab_name, $server_pubkey, $current_user_id);
-    KeyMgmt::add_key_mgmt($key);
-    $key = KeyMgmt::getByLabName($key_lab_name);
-    $key_id = $key->ID;
+    ob_start();
+    system($curl_cmd, $return_code);
+    $output = ob_get_contents();
+    ob_end_clean();
 
-    $lc_update_query = "UPDATE lab_config SET blis_cloud_hostname = '" . db_escape($connect_url) . "',"
-                     . "blis_cloud_connection_key = '" . db_escape($new_conncode) . "',"
-                     . "blis_cloud_server_pubkey_id = $key_id WHERE lab_config.lab_config_id = $lab_config_id";
+    if ($return_code == 0 && strlen($output) > 0) {
+        // HACK HACK HACKITY HACK
+        // If there is ever a space in the output we're in trouble!
+        // Really we're in trouble for a lot of reasons with this code.
+        $resp_beginning = strpos($output, "{\"");
+        $parseable_output = substr($output, $resp_beginning);
 
-    query_update($lc_update_query);
+        $r_json = json_decode($parseable_output, true);
 
-    $_SESSION['FLASH'] = "Connection established successfully.";
+        $new_conncode = $r_json["connection_code"];
+        $server_pubkey = base64_decode($r_json["public_key"]);
+
+        $key_lab_name = "BLIS Cloud Connection for $lab_config_name";
+        $key = KeyMgmt::create($key_lab_name, $server_pubkey, $current_user_id);
+        KeyMgmt::add_key_mgmt($key);
+        $key = KeyMgmt::getByLabName($key_lab_name);
+        $key_id = $key->ID;
+
+        $lc_update_query = "UPDATE lab_config SET blis_cloud_hostname = '" . db_escape($connect_url) . "',"
+                         . "blis_cloud_connection_key = '" . db_escape($new_conncode) . "',"
+                         . "blis_cloud_server_pubkey_id = $key_id WHERE lab_config.lab_config_id = $lab_config_id";
+
+        query_update($lc_update_query);
+
+        $failure = false;
+        break;
+    } else {
+        if (strlen($output) == 0) {
+            $outstr = "Output is empty!";
+        } else {
+            $outstr = "Output: $output";
+        }
+        $log->warn("Request failed. Curl exit code: $return_code. $outstr");
+        $failure = true;
+    }
 }
 
-curl_close($curl);
+if ($failure) {
+    $_SESSION['FLASH'] = "There was an error establishing the connection. Check the logs for details.";
+} else {
+    $_SESSION['FLASH'] = "Connection established successfully.";
+}
 
 header("Location: /lab_config_home.php?id=$lab_config_id");
