d6011ce127fada90e8895721aeff89f0b8b5fe01
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2025-04-18 04:10:08 +0000

    Use INSERT IGNORE for user type migration

--- db/migrations/revamp/20250225803000_addsatellitelabusertype.sql
+++ db/migrations/revamp/20250225803000_addsatellitelabusertype.sql
@@ -1,1 +1,4 @@
-INSERT INTO `user_type` (`level`, `name`, `defaultdisplay`, `created_by`, `created_on`) VALUES ('20', 'SATELLITE_LAB', '1', 'Princesca', CURRENT_TIMESTAMP);
+-- Using INSERT IGNORE - it's important to have this user type,
+-- but it might conflict with what's already in the table and we
+-- don't have a sophisticated way to deal with that yet.
+INSERT IGNORE INTO `user_type` (`level`, `name`, `defaultdisplay`, `created_by`, `created_on`) VALUES ('20', 'SATELLITE_LAB', '1', 'Princesca', CURRENT_TIMESTAMP);
