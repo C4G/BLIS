d5def9015064620be996f7816317a98a2e744c18
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2026-02-08 03:37:53 +0000

    Add php-cli too

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -7,7 +7,7 @@ echo "export PATH=\"$HOME/.local/bin:\$PATH\"" | tee -a "$HOME/.bashrc"
 source "$HOME/.bashrc"
 
 sudo apt-get update
-sudo apt-get install -y python3 python3-venv libpango-1.0-0 libpangoft2-1.0-0
+sudo apt-get install -y python3 python3-venv libpango-1.0-0 libpangoft2-1.0-0 php-cli
 python3 -m venv ~/.mkdocs/
 ~/.mkdocs/bin/pip3 install -r requirements.txt
 mkdir -p ~/.local/bin && ln -s ~/.mkdocs/bin/mkdocs ~/.local/bin/mkdocs
