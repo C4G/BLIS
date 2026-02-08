f71d5c4794a8ad81a361c07f41ba098bcff55bf9
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2026-02-08 03:35:58 +0000

    Install extra packages and mkdocs in post-create-command

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
+
 echo "export PATH=\"$HOME/.local/bin:\$PATH\"" | tee -a "$HOME/.bashrc"
 # shellcheck disable=SC1091
 source "$HOME/.bashrc"
 
-#python3 -m venv ~/venv-mkdocs
-#~/venv-mkdocs/bin/pip3 install -r requirements.txt
-#mkdir -p ~/.local/bin && ln -s ~/venv-mkdocs/bin/mkdocs ~/.local/bin/mkdocs
-
+sudo apt-get update
+sudo apt-get install -y python3 python3-venv libpango-1.0-0 libpangoft2-1.0-0
+python3 -m venv ~/.mkdocs/
+~/.mkdocs/bin/pip3 install -r requirements.txt
+mkdir -p ~/.local/bin && ln -s ~/.mkdocs/bin/mkdocs ~/.local/bin/mkdocs
