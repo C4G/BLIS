d015649778c0daa7742658332ab42cee46174147
Haki Atalov <haki.atalov@gmail.com>
2026-03-06 22:42:12 -0500

    Adds bug fixes

--- htdocs/includes/db_lib.php
+++ htdocs/includes/db_lib.php
@@ -6470,6 +6470,7 @@ function add_patient($patient, $importOn = false)
 	$created_by = db_escape($patient->createdBy);
 	$hash_value = $patient->generateHashValue();
 	$satellite_lab_id = db_escape($patient->satelliteLabId);
+	$satellite_lab_id_query = ($satellite_lab_id === "") ? "NULL" : "'$satellite_lab_id'";
 	$satellite_lab_name = db_escape($patient->satelliteLabName);
 
 	$query_string = "";
@@ -6485,22 +6486,22 @@ function add_patient($patient, $importOn = false)
 	{
 		$query_string =
 			"INSERT INTO `patient`(`patient_id`, `addl_id`, `name`, `age`, `sex`, `surr_id`, `created_by`, `hash_value` ,`ts`, `satellite_lab_id`, `satellite_lab_name`) ".
-			"VALUES ($pid, '$addl_id', '$name', $age, '$sex', '$surr_id', $created_by, '$hash_value', '$receipt_date', '$satellite_lab_id', '$satellite_lab_name')";
+			"VALUES ($pid, '$addl_id', '$name', $age, '$sex', '$surr_id', $created_by, '$hash_value', '$receipt_date', $satellite_lab_id_query, '$satellite_lab_name')";
 	}
 	else if($partial_dob != "")
 	{
 		$query_string =
 			"INSERT INTO `patient`(`patient_id`, `addl_id`, `name`, `age`, `sex`, `partial_dob`, `surr_id`, `created_by`, `hash_value`,`ts`, `satellite_lab_id`, `satellite_lab_name`) ".
-			"VALUES ($pid, '$addl_id', '$name', $age, '$sex', '$partial_dob', '$surr_id', $created_by, '$hash_value', '$receipt_date', '$satellite_lab_id', '$satellite_lab_name')";
+			"VALUES ($pid, '$addl_id', '$name', $age, '$sex', '$partial_dob', '$surr_id', $created_by, '$hash_value', '$receipt_date', $satellite_lab_id_query, '$satellite_lab_name')";
 	}
 	else
 	{
 		$query_string =
 			"INSERT INTO `patient`(`patient_id`, `addl_id`, `name`, `dob`, `age`, `sex`, `surr_id`, `created_by`, `hash_value`, `ts`, `satellite_lab_id`, `satellite_lab_name`) ".
-			"VALUES ($pid, '$addl_id', '$name', '$dob', $age, '$sex', '$surr_id', $created_by, '$hash_value', '$receipt_date', '$satellite_lab_id', '$satellite_lab_name')";
+			"VALUES ($pid, '$addl_id', '$name', '$dob', $age, '$sex', '$surr_id', $created_by, '$hash_value', '$receipt_date', $satellite_lab_id_query, '$satellite_lab_name')";
 	}
 
-	print $query_string;
+	// print $query_string;
 	query_insert_one($query_string);
 	return true;
 }
